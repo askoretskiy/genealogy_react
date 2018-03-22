43b8a2cf83776597f1e89f5686105e2e0e227ba2
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2018-03-22 22:52:56 +0100

    Added dates formatting

--- frontend/main_page.js
+++ frontend/main_page.js
@@ -11,18 +11,27 @@ const onDomLoad = () => new Promise((resolve, reject) => {
     document.addEventListener('DOMContentLoaded', resolve);
 });
 
+const PEOPLE_PREPARED = PEOPLE.map(person => ({
+    ...person,
+    birth: person.birth ? new Date(person.birth) : null,
+    death: person.death ? new Date(person.death) : null,
+}));
+
 
 const get_month_part = d => [d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()];
 
+const TODAY = new Date();
 
-const get_age = date => {
-    const today = new Date();
-    if (typeof date === "string") {
-        date = new Date(date);
+
+const years_diff = (start, end) => {
+    if (typeof start === "string") {
+        start = new Date(start);
     }
-    let diff = (get_month_part(today) < get_month_part(date)) ? 0 : 1;
-    return today.getFullYear() - date.getFullYear() - diff;
+    let diff = (get_month_part(end) < get_month_part(start)) ? 0 : 1;
+    return end.getFullYear() - start.getFullYear() - diff;
 };
+
+const format_date = date => `${('' + date.getDate()).padStart(2, '0')}.${('' + (date.getMonth()+1)).padStart(2, '0')}.${date.getFullYear()}`;
 
 
 onDomLoad().then(() => {
@@ -41,9 +50,12 @@ const Application = () => [
 
 const People = () => (
     <ul>
-        {PEOPLE.map(person => (
+        {PEOPLE_PREPARED.map(person => (
             <li key={person.id}>
-                {person.last_name} {person.first_name} {person.middle_name} ({get_age(person.birth)})
+                {person.last_name} {person.first_name} {person.middle_name}
+                {' '}<span title={`${format_date(person.birth)}${!person.death ? '' : ' - '+format_date(person.death)}`}>
+                    ({years_diff(person.birth, person.death || TODAY)})
+                </span>
             </li>
         ))}
     </ul>
