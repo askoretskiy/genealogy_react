d2a31022887f88cd02d2f3782afdb310d62270df
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2018-03-22 22:35:58 +0100

    Show list of persons from people.yaml

--- frontend/main_page.js
+++ frontend/main_page.js
@@ -3,20 +3,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import PEOPLE from '/home/data/people.yaml';
+
 import 'normalize.css';
 
 const onDomLoad = () => new Promise((resolve, reject) => {
     document.addEventListener('DOMContentLoaded', resolve);
 });
 
+
+const get_month_part = d => [d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()];
+
+
+const get_age = date => {
+    const today = new Date();
+    if (typeof date === "string") {
+        date = new Date(date);
+    }
+    let diff = (get_month_part(today) < get_month_part(date)) ? 0 : 1;
+    return today.getFullYear() - date.getFullYear() - diff;
+};
+
+
 onDomLoad().then(() => {
     let div = document.createElement('div');
     document.body.appendChild(div);
-    ReactDOM.render(Application, div);
+    ReactDOM.render((
+        <Application/>
+    ), div);
 });
 
-const Application = (
-    <div>
-        Hello world!
-    </div>
+
+const Application = () => [
+    <h1 key={1}>Genealogy</h1>,
+    <People key={2}/>,
+];
+
+const People = () => (
+    <ul>
+        {PEOPLE.map(person => (
+            <li key={person.id}>
+                {person.last_name} {person.first_name} {person.middle_name} ({get_age(person.birth)})
+            </li>
+        ))}
+    </ul>
 );
