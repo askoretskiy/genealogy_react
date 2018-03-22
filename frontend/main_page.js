aac079ba91f3e89092519ed463d11f228224eb46
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2018-03-22 23:55:51 +0100

    Initial version of tree

--- frontend/main_page.js
+++ frontend/main_page.js
@@ -17,16 +17,27 @@ const PEOPLE_PREPARED = PEOPLE.map(person => ({
     ...person,
     birth: person.birth ? new Date(person.birth) : null,
     death: person.death ? new Date(person.death) : null,
+    children: [],
 }));
 
+const PEOPLE_INDEX = new Map(PEOPLE_PREPARED.map(person => [person.id, person]));
+
+for (let person of PEOPLE_INDEX.values()) {
+    person.parents = person.parents.filter(id => id !== null).map(id => PEOPLE_INDEX.get(id));
+    for (let parent of person.parents) {
+        parent.children.push(person);
+    }
+}
+
+const PEOPLE_TOP = [...PEOPLE_INDEX.values()].filter(person => !person.parents.length);
 
 const get_month_part = d => [d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()];
 
 const compare_months = (start, end) => {
     const start_month = get_month_part(start);
     const end_month = get_month_part(end);
-    for(let i=0;i<start_month.length;i++) {
-        if(end_month[i] < start_month[i]) {
+    for (let i = 0; i < start_month.length; i++) {
+        if (end_month[i] < start_month[i]) {
             return false;
         }
     }
@@ -35,7 +46,7 @@ const compare_months = (start, end) => {
 
 const years_diff = (start, end) => end.getFullYear() - start.getFullYear() - (compare_months(start, end) ? 0 : 1);
 
-const format_date = date => `${('' + date.getDate()).padStart(2, '0')}.${('' + (date.getMonth()+1)).padStart(2, '0')}.${date.getFullYear()}`;
+const format_date = date => `${('' + date.getDate()).padStart(2, '0')}.${('' + (date.getMonth() + 1)).padStart(2, '0')}.${date.getFullYear()}`;
 
 
 onDomLoad().then(() => {
@@ -49,17 +60,21 @@ onDomLoad().then(() => {
 
 const Application = () => [
     <h1 key={1}>Genealogy</h1>,
-    <People key={2}/>,
+    <People key={2} people={PEOPLE_TOP}/>,
 ];
 
-const People = () => (
+const People = ({people}) => (
     <ul>
-        {PEOPLE_PREPARED.map(person => (
+        {people.map(person => (
             <li key={person.id}>
                 {person.last_name} {person.first_name} {person.middle_name}
-                {' '}<span title={`${format_date(person.birth)}${!person.death ? '' : ' - '+format_date(person.death)}`}>
+                {' '}
+                <span title={`${format_date(person.birth)}${!person.death ? '' : ' - ' + format_date(person.death)}`}>
                     ({years_diff(person.birth, person.death || TODAY)})
                 </span>
+                {!person.children.length ? null : (
+                    <People people={person.children}/>
+                )}
             </li>
         ))}
     </ul>
